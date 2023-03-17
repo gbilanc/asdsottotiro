b361fd18c1037e9f4c5f02bf53739131a17d622c
gbilanc <gbilanc@gmail.com>
2023-03-17 11:50:31 +0100

    2023.03.17 menu compleanno Lucia

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ include_once 'mailer.php';
 $page = filter_input(INPUT_GET, 'p');
 $page = $page ? $page : 'page01';
 $menu = filter_input(INPUT_GET, 'm');
-
 ?>
 <html>
     <head>
@@ -37,4 +36,20 @@ echo("$('ul li:eq($menu)').addClass('current');");
         <script src="assets/js/util.js"></script>
         <script src="assets/js/main.js"></script>
     </body>
+
+    <!-- Cookie Consent by PrivacyPolicies.com https://www.PrivacyPolicies.com -->
+    <script type="text/javascript" src="//www.privacypolicies.com/public/cookie-consent/4.1.0/cookie-consent.js" charset="UTF-8"></script>
+    <script type="text/javascript" charset="UTF-8">
+            document.addEventListener('DOMContentLoaded', function () {
+                cookieconsent.run({"notice_banner_type": "simple", "consent_type": "express", "palette": "light", "language": "it", "page_load_consent_levels": ["strictly-necessary"], "notice_banner_reject_button_hide": false, "preferences_center_close_button_hide": false, "page_refresh_confirmation_buttons": false, "website_name": "www.asdsottotiro.it", "website_privacy_policy_url": "https://privacy.clion.agency/privacy.php?id=a1419921311b9940b779eb9492d96f20&dom=862"});
+            });
+    </script>
+
+    <noscript>Cookie Consent by <a href="https://www.privacypolicies.com/">Privacy Policies website</a></noscript>
+    <!-- End Cookie Consent by PrivacyPolicies.com https://www.PrivacyPolicies.com -->
+    <!-- Below is the link that users can use to open Preferences Center to change their preferences. Do not modify the ID parameter. Place it where appropriate, style it as needed. -->
+
+    <a href="#" id="open_preferences_center">Update cookies preferences</a>
+
+
 </html>
